d856146386df73105d646746aee7e33f3a49b397
jose <2023159140@ub.edu.bz>
2025-04-20 10:12:43 -0600

    task1

--- main.go
+++ main.go
@@ -8,35 +8,36 @@ import (
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	buf := make([]byte, 1024)
+
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
-			fmt.Println("Error reading from client", err)
+			fmt.Println("Error reading from client:", err)
 			return
 		}
 
 		_, err = conn.Write(buf[:n])
 		if err != nil {
-			fmt.Println("Error writing to client", err)
+			fmt.Println("Error writing to client:", err)
 		}
 	}
-
 }
 
 func main() {
+	// define the target host and port we want to connect to
 	listener, err := net.Listen("tcp", ":4000")
 	if err != nil {
 		panic(err)
 	}
 	defer listener.Close()
-	fmt.Println("Serve is listening on port :4000")
-	//our program runs an infinite loop
+	fmt.Println("Server listening on :4000")
+	// Our program runs an infinite loop
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting: ", err)
+			fmt.Println("Error accepting:", err)
 			continue
 		}
-		handleConnection(conn)
+		go handleConnection(conn)
 	}
 }
